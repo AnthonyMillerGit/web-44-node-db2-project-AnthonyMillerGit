1b1c4a011c97fb27fe797d02584a4acc70cf7586
ferengitooth <ehitsamill@gmail.com>
2021-08-11 15:30:05 -0700

    fleshed out make cars table

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1532596459853',
+        make: 'toyata',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1532596459853',
+        make: 'toyata',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '1532596459853',
+        make: 'ford',
+        model: 'focu',
+        mileage: 15000,
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
